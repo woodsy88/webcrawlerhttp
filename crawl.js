724e3d3bc2e7190e63ee4e50116d91644f21178e
Andrew Woods <andrewwoods88@gmail.com>
2023-06-09 12:20:17 -0400

    getURLS function and tests

--- crawl.js
+++ crawl.js
@@ -1,6 +1,42 @@
+const { log } = require('console');
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody)
+  const linkElements = dom.window.document.querySelectorAll('a');
+  for (const linkElement of linkElements) {
+      console.log("link element", linkElement.href);
+      // if the first character is a /, its a relative url
+      if (linkElement.href.slice(0,1) === '/') {
+          try {
+            // relative
+            const urlObj = new URL(`${baseURL}${linkElement.href}`)
+            console.log("urlString", urlObj);
+            urls.push(urlObj.href)
+          }
+          catch (error) {
+            console.log(`error with relative path: ${error.message}`);
+          }
+      } else {
+        try {
+        // absolute url
+        const urlObj = new URL(linkElement.href)
+        console.log("urlString", urlObj);
+        urls.push(urlObj.href)
+        }
+        catch (error) {
+          console.log(`error with absolute path ${error.message}`);
+        }
+      }
+  }
+  console.log("urls", urls);
+  return urls
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
-  console.log(urlObj);
+  // console.log(urlObj);
   const hostPath =`${urlObj.hostname}${urlObj.pathname}`;
   // check if last character equals a slash
   if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
@@ -13,5 +49,6 @@ function normalizeURL(urlString) {
 // normalizeURL('https://blog.boot.dev/path')`
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL strip protocal', () => {
@@ -30,5 +30,78 @@ test('normalizeURL strip http', () => {
   // actul output of our normalize function
   const actual = normalizeURL(input)
   const expected = 'blog.boot.dev/path'
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute urls', () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="https://blog.boot.dev/">
+        Boot.dev.blog
+      </a>   
+    </body>
+  </html>
+  `
+  const inputBaseURL = 'https://blog.boot.dev'
+  // actul output of our normalize function
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ["https://blog.boot.dev/"]
+  expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML relative urls', () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="/path/">
+        Boot.dev.blog
+      </a>   
+    </body>
+  </html>
+  `
+  const inputBaseURL = 'https://blog.boot.dev'
+  // actul output of our normalize function
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ["https://blog.boot.dev/path/"]
+  expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML multiple links - relative and absolute', () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="/path1/">
+        Boot.dev.blog
+      </a>  
+      <a href="https://blog.boot.dev/path2/">
+      Boot.dev.blog   
+      </a>
+    </body>
+  </html>
+  `
+  const inputBaseURL = 'https://blog.boot.dev'
+  // actul output of our normalize function
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"]
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid url', () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="invalid">
+        Invalid URL
+      </a>   
+    </body>
+  </html>
+  `
+  const inputBaseURL = 'https://blog.boot.dev'
+  // actul output of our normalize function
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = []
   expect(actual).toEqual(expected)
 })
